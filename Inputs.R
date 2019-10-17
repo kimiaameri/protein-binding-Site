9bc132ce406fc7918b2b2f8b62e29335606a3b8a
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-10-17 17:53:14 -0500

    Update Inputs.R

--- Inputs.R
+++ Inputs.R
@@ -1,5 +1,9 @@
-intesect.reference.vcf <- function(i, reference){
-   intersections <- as.matrix(read.table(paste("./input/intersections/",i,sep=""),header=F,sep="\t",stringsAsFactors = F))
+argv <- commandArgs(trailingOnly = TRUE)
+
+intesect_reference_vcf <- function(i, reference,intersectionspath){
+  file = paste0(intersectionspath,i,sep="")
+   if(file.info(file)$size > 0) {
+    intersections <- as.matrix(read.table(paste0(intersectionspath,i,sep=""),header=F,sep="\t",stringsAsFactors = F))
     length.intersection= nrow(intersections)
     length.genome= nrow(reference)
     for (j in 1 :length.genome)
@@ -8,6 +12,7 @@ intesect.reference.vcf <- function(i, reference){
           if (intersections[i,2] >= reference[j,2] & intersections[i,2] <= reference[j,3]) 
               intersections[i,4] = reference [j,4]
     }
-   
-  return(table(intersections[,4]))
+     return(table(intersections[,4]))
+  }
 }
+
